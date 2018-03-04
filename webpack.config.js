8e1c9e3123884255048fea4c1759879ee73665b4
Kabiev Kuanysh <thekabiev@gmail.com>
2018-03-04 15:25:08 +0600

    webpack config first part

--- webpack.config.js
+++ webpack.config.js
@@ -3,3 +3,22 @@ const webpack = require('webpack');
 
 const src = {root: path.resolve(__dirname, 'src')};
 const dist = {root: path.resolve(__dirname, 'dist')};
+
+module.exports = {
+    context: src.root,
+    entry: {
+        app: [
+            './js/app.js',
+        ],
+    },
+    output:{
+        filename: 'js/[name].js',
+        path: dist.root,
+        publicPath: '../'
+    },
+    module: {
+        rules: [
+
+        ],
+    },
+}
