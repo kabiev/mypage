9a56cc0c2245468f04b88c8f13804dea90b173b6
Kabiev Kuanysh <thekabiev@gmail.com>
2018-03-14 10:39:22 +0600

    add pug pages

---
+++ src/js/app.js
@@ -0,0 +1,1 @@
+requireAll(require.context('../pages', false, /\.pug$/));
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const webpack = require('webpack');
 
-const src = {root: path.resolve(__dirname, 'src')};
-const dist = {root: path.resolve(__dirname, 'dist')};
+const src = { root: path.resolve(__dirname, 'src') };
+const dist = { root: path.resolve(__dirname, 'dist') };
 
 module.exports = {
     context: src.root,
@@ -11,13 +11,51 @@ module.exports = {
             './js/app.js',
         ],
     },
-    output:{
+    output: {
         filename: 'js/[name].js',
         path: dist.root,
         publicPath: '../'
     },
     module: {
         rules: [
+            {
+                test:/\.js$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg|woff)$/,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options: {
+                      name: '[path][name].[ext]'
+                    }
+                  }
+                ]
+              },
+            {
+                test: /\.pug$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[path][name].html'
+                        },
+                    },
+                    {
+                        loader: 'extract-loader'
+                    },
+                    {
+                        loader: 'html-loader'
+                    },
+                    {
+                        loader: 'pug-html-loader',
+                        options: {
+                            pretty: true,
+                        },  
+                    },
+                ],
+            },
 
         ],
     },
